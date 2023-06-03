2fd815ba77b79682dee1c254ecf19a1487e236a7
wellerson <wellersonjava@gmail.com>
2023-06-03 11:02:00 -0300

    Adição de alguns classes para o programa

---
+++ src/main/java/db/Conexao.java
@@ -0,0 +1,25 @@
+package db;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author wellersonelucas
+ */
+public class Conexao {
+    public static Connection fazConexao() throws SQLException{
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+
+            return DriverManager.getConnection("jdbc:mysql://localhost/db_leafless", "root","lusim123");
+
+        }catch(ClassNotFoundException e){
+
+            throw new SQLException(e.getException());
+        }
+
+    }
+}
--- src/main/java/db/ConnectionFactory.java
+++ src/main/java/db/ConnectionFactory.java
@@ -1,16 +1,16 @@
+package db;
 
- import java.sql.Connection;
- import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
     public class ConnectionFactory {
 
         private static String host = "localhost";
         private static String porta = "3306";
-        private static String db = "seubd";
-        private static String usuario = "seusuario";
-        private static String senha = "suasenha";
-
-        public static Connection obterConexao() throws Exception {
+        private static String db = "db_leafless";
+        private static String usuario = "root";
+        private static String senha = "lusim123";
+        public static Connection obterConexao () throws Exception{
             String url = String.format(
                     "jdbc:mysql://%s:%s/%s",
                     host,
